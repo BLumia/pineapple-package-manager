ba453bd46583f7b0cf4e384b66948baf6fc58adf
Gary Wang <wzc782970009@gmail.com>
2022-01-05 18:47:00 +0800

    basic workflow test (without actual extracting file now)

--- main.cpp
+++ main.cpp
@@ -1,28 +1,106 @@
-#include <zip.h>
-int main()
-{
-    //Open the ZIP archive
-    int err = 0;
-    zip *z = zip_open("foo.zip", 0, &err);
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <set>
 
-    //Search for the file of given name
+#include <zip.h>
+
+// return the value of the given environment variable name, or empty if not exist.
+// please notice the value of an existing env var could also be empty.
+std::string getEnv(std::string env_name) {
+    std::string envname = env_name.substr(1, env_name.length() - 2);
+    const char * envcstr = std::getenv(envname.c_str());
+    std::string env(envcstr == nullptr ? "" : envcstr);
+    return env;
+}
+
+std::string_view getBaseDirName(std::string_view path) {
+    std::string_view::size_type pos = path.find_first_of('/'); // won't be npos
+    if (pos == std::string_view::npos) {
+        return {};
+    }
+    std::string_view dirname = path.substr(0, pos);
+
+    return dirname;
+}
+
+// get the base dir name of a given path, and also return if it's name is an environment variable name
+// the given path MUST be a folder path (ends with '/')
+std::pair<bool, std::string> getEnvBaseDirName(std::string_view path) {
+    if (path.length() <= 2 || path.at(path.length() - 1) != '/') return std::make_pair(false, std::string());
+
+    std::string_view dirname = getBaseDirName(path);
+
+    bool isEnvDir = (dirname.length() > 2 && dirname.at(0) == '%' && dirname.at(dirname.length() - 1) == '%');
+    return std::make_pair(isEnvDir, std::string(dirname));
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        std::cout << "[Usage] " << argv[0] << " archive.zip" << std::endl;
+        return -1;
+    }
+
+    const char* fileName = argv[1];
+
+    // Open the ZIP archive
+    int err = 0;
+    zip *z = zip_open(fileName, 0, &err);
+    if (!z) {
+        zip_error_t et;
+        zip_error_init_with_code(&et, err);
+        printf("zip_open failed (%d: %s)", err, zip_error_strerror(&et));
+        zip_error_fini(&et);
+        return -1;
+    }
+
+    // Search for the file of given name
     const char *name = "file.txt";
     struct zip_stat st;
     zip_stat_init(&st);
-    zip_stat(z, name, 0, &st);
+    int index = zip_stat(z, name, 0, &st);
+    if (index != -1) {
+        // Alloc memory for its uncompressed contents
+        // it's a text file so we should add the \0 manually...
+        char *contents = new char[st.size + 1];
+        contents[st.size] = '\0';
 
-    //Alloc memory for its uncompressed contents
-    char *contents = new char[st.size];
+        // Read the compressed file
+        zip_file *f = zip_fopen(z, "file.txt", 0);
+        zip_fread(f, contents, st.size);
 
-    //Read the compressed file
-    zip_file *f = zip_fopen(z, "file.txt", 0);
-    zip_fread(f, contents, st.size);
+        puts("=== start ===");
+        printf("%s", contents);
+        puts("===  end  ===");
 
-    puts("=== start ===");
-    printf("%s", contents);
-    puts("===  end  ===");
+        zip_fclose(f);
+    }
 
-    zip_fclose(f);
+    // get all dirs at archive root with %SAMPLE% as name
+    zip_int64_t num_entries = zip_get_num_entries(z, 0);
+    std::set<std::string> paths;
+    for (zip_uint64_t i = 0; i < (zip_uint64_t)num_entries; i++) {
+        const char *name = zip_get_name(z, i, 0);
+
+        bool isEnvDir;
+        std::string dirName;
+        std::tie(isEnvDir, dirName) = getEnvBaseDirName(name);
+
+        if (isEnvDir) {
+            paths.insert(dirName);
+        }
+    }
+    for (std::string singlePath : paths) {
+        std::string env_val = getEnv(singlePath);
+        std::cout << "> " << singlePath << env_val << std::endl;
+        if (env_val.empty()) {
+            std::cout << "> !! empty" << std::endl;
+        } else {
+            bool exist = std::filesystem::exists(env_val);
+            std::cout << "> " << (exist ? "exist" : "NOT exist") << std::endl;
+        }
+    }
 
     //And close the archive
     zip_close(z);
